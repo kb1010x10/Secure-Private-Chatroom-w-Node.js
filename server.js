911c2ab444e226a4acc12634d4f776ecb9a43d08
kb1010x10 <69879762+kb1010x10@users.noreply.github.com>
2020-09-02 16:52:43 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var urlencodedParser = bodyParser.urlencoded({extended : false})
+var app = express();
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+var messages=[]
+var users=[]
+
+app.get('/', (req, res) => {
+   res.sendFile('index.html', {root: __dirname })
+})
+
+app.post('/send_message', urlencodedParser, (req,res) => {
+   messages.push(req.body)
+   io.emit('message')
+   res.sendStatus(200)
+})
+
+app.get('/messages', (req,res)=>{
+   res.send(JSON.stringify(messages))
+})
+
+io.on('connection', (socket)=>{
+   var userId;
+	
+   console.log('A user connected.')
+   
+   socket.on('setUserName', (data)=>{
+      if(users.indexOf(data)==-1){
+		if(data[0]=='A') {
+			data = data.substring(1);
+			users.push(data);
+			userId = data;
+			console.log(data + " joined the chat!");
+			io.emit('is_online', data, '<i>' + data + ' joined the chat!</i>');
+			socket.emit('userSet', {username:data});
+		}
+		else {
+			console.log(data + ' tried to join the chat.');
+			io.emit('is_online', data, '<b>' + data + ' tried to join the chat.</b>');
+		} 
+		 
+      }else{
+         console.log('server mai')
+         socket.emit('userExists', `${data} already exists, please change your Username to join the chat`)
+      }
+   })
+   socket.on('typing', (data)=>{
+      if(data.typing==true)
+         io.emit('display', data)
+      else
+         io.emit('display', data)
+   })
+   
+   socket.on('disconnect', (data)=> {
+	  console.log(userId + ' left the chat.')
+	  
+	  io.emit('is_online', userId, '<i>' + userId + ' left the chat.</i>')
+   })
+})
+
+/*io.on('disconnect', (socket)=>{
+	console.log('A user disconnected')
+	
+	io.emit('is_online', '... left the chat!')
+})*/
+
+const port = process.env.PORT || 3000
+var server = http.listen(port, () => {
+   console.log(`app listening at port ${server.address().port}`)
+})
